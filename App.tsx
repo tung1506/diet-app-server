93e013453981dc69c660308c5e03711ba9d91de4
bss <tung15.6.2003@gmail.com>
2024-10-24 13:40:19 +0700

    client

--- App.tsx
+++ App.tsx
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Auth from './components/Auth';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Sign In with Google</Text>
+      <Auth>
+      </Auth>
       <StatusBar style="auto" />
     </View>
   );
